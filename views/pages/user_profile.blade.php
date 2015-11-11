4456a606355539224cd688f75a0bea8cf9e6f4cc
françois varnier <groovelcms@gmail.com>
2015-11-11 14:40:28 +0100

    GROOVEL_DEV:fix template user profile

--- views/pages/user_profile.blade.php
+++ views/pages/user_profile.blade.php
@@ -17,56 +17,8 @@
 		<h1>{!!Session::get('user')['username']!!}</h1>
 		<input type="hidden" id="user" value={!!Session::get('user')['id']!!}>
 
-		<!--<nav id="profiletabs">
-			<ul class="clearfix">
-				<li><a href="#settings">Settings</a></li>
-			</ul>
-		</nav>
 
-		<section id="bio">
-			<p>
-				Various content snippets courtesy of <a
-					href="http://bluthipsum.com/">Bluth Ipsum</a>.
-			</p>
-
-			<p>Can't a guy call his mother pretty without it seeming strange?
-				Amen. I think that's one of Mom's little fibs, you know, like I'll
-				sacrifice anything for my children.</p>
-
-			<p>She's always got to wedge herself in the middle of us so that she
-				can control everything. Yeah. Mom's awesome. I run a pretty tight
-				ship around here. With a pool table.</p>
-		</section>
-
-			 <section id="activity" class="hidden">
-			<p>Most recent actions:</p>
-
-			<p class="activity">@10:15PM - Submitted a news article</p>
-
-			<p class="activity">@9:50PM - Submitted a news article</p>
-
-			<p class="activity">@8:15PM - Posted a comment</p>
-
-			<p class="activity">
-				@4:30PM - Added <strong>someusername</strong> as a friend
-			</p>
-
-			<p class="activity">@12:30PM - Submitted a news article</p>
-		</section>
-
-	<section id="friends" class="hidden">
-			<p>Friends list:</p>
-
-			<ul id="friendslist" class="clearfix">
-				<li><a href="#"><img src="images/avatar.png" width="22" height="22">
-						Username</a></li>
-				<li><a href="#"><img src="images/avatar.png" width="22" height="22">
-						SomeGuy123</a></li>
-				<li><a href="#"><img src="images/avatar.png" width="22" height="22">
-						PurpleGiraffe</a></li>
-			</ul>
-		</section>-->
-	<div class="col-md-12" 
+	<div class="col-md-12"> 
 		<section id="settings" >
 			
 			<p class="setting">
@@ -94,14 +46,6 @@
 				 {!!Session::get('user')['email']!!}
 				@endif
 			</p>
-			<!--<p class="setting">
-			<span>Country</span>
-				@if(Session::get('user')['email']==null)
-				N/A
-				@else
-				 {!!Session::get('user')['email']!!}
-				@endif
-			</p>-->
 		</section>
 		</div>
 	</div>
