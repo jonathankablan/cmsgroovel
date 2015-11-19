7676f6801aad708f2e8358005f0158b463e0a208
françois varnier <groovelcms@gmail.com>
2015-11-19 12:19:26 +0100

    fix email.blade template

--- views/pages/email/email.blade.php
+++ views/pages/email/email.blade.php
@@ -1,10 +1,4 @@
 <!--This is a blade template that goes in email message -->
-<?php
-$body=\Session::get('body');
-$pseudo=\Session::get('pseudo');
-$author=\Session::get('author');
-?> 
-
 <h1>Hi {!!$pseudo!!} </h1>
 
 <p>
