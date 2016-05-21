86c3947fb6404ee0b1d98d4635986f32011cd330
françois varnier <groovelcms@gmail.com>
2016-05-21 18:38:51 +0200

    search list responsiv design

--- views/layouts/groovel_admin_search.blade.php
+++ views/layouts/groovel_admin_search.blade.php
@@ -21,7 +21,7 @@
       	    @endif
 		@endif
       <div class="row">
-        <div class="col-xs-12 col-md-12 col-md-offset-2">
+         <div class="col-sm-9 col-sm-offset-3 col-xs-12 col-md-9 col-md-offset-0">
  			   @yield('content')
        </div>
       </div>
--- views/pages/admin_list_results_search.blade.php
+++ views/pages/admin_list_results_search.blade.php
@@ -1,22 +1,19 @@
 @extends('cmsgroovel.layouts.groovel_admin_search')
 @section('content')
-	  <div class="col-md-12" style="margin-top:70px">
+	  <div class="row" style="margin-top:70px">
 	       	@if(Session::get('messages'))
 	             <div>{!!var_dump(Session::get('messages'))!!}</div>
 	        @endif
 	        
-	           <div class="table-responsive scrollable" id='contents'>
-	                 <div class="col-md-12">
-	                    	<table class="table table-striped table-hover table-bordered" id="table_contents">
+	           <div class="table table-hover table-striped table-bordered table-responsive" id='contents'>
+	                  	<table class="table table-striped table-hover table-bordered" id="table_contents">
 										<thead>
 											 <tr>
-					                          <th id="col_content" class="col-md-1">refid</th>
-					                          <th id="col_content" class="col-md-1">title</th>
-					                          <th id="col_content" class="col-md-1">type</th>
-					                          <th id="col_content" class="col-md-1">data</th>
-					                          <th id="col_content" class="col-md-1">description</th>
-					                          <th id="col_content" class="col-md-1">date creation</th>
-					                          <th id="col_content" class="col-md-1">last update</th>
+					                          <th id="col_content" class="col-xs-1 col-md-1">title</th>
+					                          <th id="col_content" class="col-xs-1 col-md-1">type</th>
+					                          <th id="col_content" class="col-xs-1 col-md-1">data</th>
+					                          <th id="col_content" class="col-xs-1 col-md-1">description</th>
+					                          <th id="col_content" class="col-xs-1 col-md-1">last update</th>
 					                        
                         					</tr>
 										</thead>
@@ -24,15 +21,13 @@
 										    @if($results!=null)
 										 	 @foreach ($results as $content)
 										 	      <tr id="rows_contents">
-						                             <td id="row_content" class="col-md-1">{!!$content['refid']!!}</td>
-						                             <td id="row_content" class="col-md-1">{!!$content['title']!!}</td>
-						                             <td id="row_content" class="col-md-1">{!!$content['type']!!}</td>
-						                             <td id="row_content" class="col-md-1">{!!$content['data']!!}</td>
-						                             <td id="row_content" class="col-md-1">{!!$content['description']!!}</td>
-						                              <td id="row_content" class="col-md-1">{!!$content['created_at']!!}</td>
-						                             <td id="row_content" class="col-md-1">{!!$content['updated_at']!!}</td>
-						                             <td id="edit" class="col-md-1">
-						                            {!! HTML::image('groovel/cmsgroovel/groovel/admin/images/edit.jpg', $alt="edit", $attributes = array('id' => 'editButton','style'=>'width:20px;height:20px','class'=>'btnSearch')) !!}
+						                            <td id="row_content" class="col-xs-1 col-md-1">{!!$content['title']!!}</td>
+						                            <td id="row_content" class="col-xs-1 col-md-1">{!!$content['type']!!}</td>
+						                            <td id="row_content" class="col-xs-1 col-md-1">{!!$content['data']!!}</td>
+						                            <td id="row_content" class="col-xs-1 col-md-1">{!!$content['description']!!}</td>
+						                            <td id="row_content" class="col-xs-1 col-md-1">{!!$content['updated_at']!!}</td>
+						                            <td id="edit" class='col-sm-4 col-lg-1 col-xs-1'>
+						                            	<span id='editButton' class="glyphicon glyphicon-pencil btnSearch" aria-hidden="true"></span>
 						                            </td>
 						                           </tr>
 					                          @endforeach
@@ -42,8 +37,7 @@
           							   <ul class="pagination">
 						                {!! $results->render() !!}
 	               					 </ul>
-	                	</div>
- 					</div>
+	    			</div>
  	   		 </div>
     <script>
  var options = {
